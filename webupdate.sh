886eb17156b4a673cb6e5e82f37495270802beb2
Johannes Kalliauer <jo.spam89@gmx.at>
2019-03-23 21:25:04 +0100

    Update webupdate.sh

--- webupdate.sh
+++ webupdate.sh
@@ -3,16 +3,18 @@
 rm master.zip
 rm -f master.zip.1
 rm -r public_html/
+rm -r convert-master/
 
-wget -q https://github.com/JoKalliauer/convert/archive/master.zip
+wget -q https://github.com/JoKalliauer/convert/archive/master.zip -O master.zip
 
-unzip -oz master.zip
+unzip -oq master.zip
 
-cp -Tr ./convert-master ./public_html
+mkdir ./public_html/
+
+mv -f ./convert-master/* ./public_html
 
 chmod a+x public_html/WorkaroundBotsvg2validsvg.sh
 
 cp public_html/webupdate.sh ./webupdate.sh
 
 chmod a+x ./webupdate.sh
-

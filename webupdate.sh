4775fa7884d36f0f2ad5088b2820a88e116ff9ae
Johannes Kalliauer <jo.spam89@gmx.at>
2019-03-23 20:45:04 +0100

    Intent

--- webupdate.sh
+++ webupdate.sh
@@ -7,7 +7,7 @@ wget https://github.com/JoKalliauer/convert/archive/master.zip
 
 unzip -o master.zip
 
-mv ./convert-master ./public_html
+mv -T ./convert-master ./public_html
 
 chmod a+x public_html/WorkaroundBotsvg2validsvg.sh
 

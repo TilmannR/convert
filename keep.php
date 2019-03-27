3da23d580c3a47b1dea935d428fa87c3ba204a67
Johannes Kalliauer <jo.spam89@gmx.at>
2019-03-27 22:57:45 +0100

    header('X-Accel-Buffering: no');

--- keep.php
+++ keep.php
@@ -1,10 +1,15 @@
 <?php
+// Setting this header instructs Nginx to disable fastcgi_buffering and disable
+// gzip for this request.
+header('X-Accel-Buffering: no');
+
 $tool_user_name = 'svgworkaroundbot';
 include_once ( 'shared/common.php' ) ;
 error_reporting( E_ALL & ~E_NOTICE ); # Don't clutter the directory with unhelpful stuff
 $url = getProtocol() . "://tools.wmflabs.org/$tool_user_name/";
 if ( !array_key_exists( 'file', $_FILES ) ) {
   header( "Location: $url" );
+  header('X-Accel-Buffering: no');
   die();
 }
 $uploadName = $_FILES['file']['tmp_name'];
@@ -13,11 +18,13 @@ $targetName = $fileName . '.svg';
 if ( $_FILES['file']['size'] > 5*0x100000 ) {
   unlink( $uploadName );
   header( "Location: $url#tooBig" );
+  header('X-Accel-Buffering: no');
   die();
 }
 if ( !move_uploaded_file( $uploadName, $fileName ) ) {
   unlink( $uploadName );
   header( "Location: $url#cantmove" );
+  header('X-Accel-Buffering: no');
   echo( 'cant move uploaded file' );
   die();
 }
@@ -33,6 +40,7 @@ file_put_contents($file, $current);
 $handle = fopen( $targetName, 'r' );
 if ( $handle === false ) {
   header( "Location: $url#conversionError" );
+  header('X-Accel-Buffering: no');
   echo( 'error converting the file' );
   die();
 }
@@ -40,6 +48,7 @@ if ( filesize( $targetName ) > 10*0x100000 ) {
   fclose( $handle );
   unlink( $targetName );
   header( "Location: $url#outputTooHuge" );
+  header('X-Accel-Buffering: no');
   echo( 'output unexpectedly huge' );
   die();
 }
@@ -48,11 +57,13 @@ fclose( $handle );
 unlink( $targetName );
 if ( strlen( $content ) > 10*0x100000 ) {
   header( "Location: $url#outputTooHuge2" );
+  header('X-Accel-Buffering: no');
   echo( 'output unexpectedly huge 2' );
   die();
 }
 header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' );
 header( 'Content-type: image/svg+xml' );
 header( 'Content-Disposition: attachment; filename="' . addslashes( $_FILES['file']['name'] ) . '.svg"' );
+header('X-Accel-Buffering: no');
 echo $content;
 die();

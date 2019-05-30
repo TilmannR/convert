5b1a3a2239b2d6c498c1bb50d785d7391440b15c
Johannes Kalliauer <jo.spam89@gmx.at>
2019-05-30 23:36:43 +0200

    tmp to $2

--- WorkaroundBotsvg2validsvg.sh
+++ WorkaroundBotsvg2validsvg.sh
@@ -76,9 +76,9 @@ if [ $ScourScour = 'YES' ]; then
  export scour
  echo runScourScour $ScourJK $ScourScour
  #rm tmp.svg
- $ScourJK -i $i -o tmp.svg --keep-unreferenced-defs --remove-descriptions --strip-xml-space  --set-precision=6 --indent=space --nindent=1 --renderer-workaround --disable-style-to-xml  --set-c-precision=6 --protect-ids-noninkscape  --disable-simplify-colors  --keep-editor-data --error-on-flowtext # --enable-comment-stripping --create-groups  #--enable-viewboxing #
+ $ScourJK -i $i -o $2 --keep-unreferenced-defs --remove-descriptions --strip-xml-space  --set-precision=6 --indent=space --nindent=1 --renderer-workaround --disable-style-to-xml  --set-c-precision=6 --protect-ids-noninkscape  --disable-simplify-colors  --keep-editor-data --error-on-flowtext # --enable-comment-stripping --create-groups  #--enable-viewboxing #
  rm $i
- mv tmp.svg $i
+ mv $2 $i
 else 
  echo no ScourScour $ScourScour
 fi

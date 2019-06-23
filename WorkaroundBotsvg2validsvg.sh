f94aad9a803e59295dd1f3eff176e640b35ff26c
Johannes Kalliauer <jo.spam89@gmx.at>
2019-06-23 20:35:06 +0200

    Update WorkaroundBotsvg2validsvg.sh
    /data/project/svgworkaroundbot/prgm2/pythonJK/PythonIn/bin/python3.7 -m scour.scour

--- WorkaroundBotsvg2validsvg.sh
+++ WorkaroundBotsvg2validsvg.sh
@@ -94,8 +94,8 @@ if [ $ScourScour = 'YES' ]; then
  echo runScourScour,JK $ScourJK, YN $ScourScour, i $i ,ii $i2 >foobar93
  #rm tmp.svg
  cp $i Output095.svg
- python3 -m scour.scour -i $i -o $i2 --keep-unreferenced-defs --remove-descriptions --strip-xml-space  --set-precision=6 --indent=space --nindent=1 --renderer-workaround --set-c-precision=6 --protect-ids-noninkscape  --disable-simplify-colors  --keep-editor-data >foobar097
- python3 -m scour.scour -i tmpJK2.svg -o tmpJK23.svg --keep-unreferenced-defs --remove-descriptions --strip-xml-space  --set-precision=6 --indent=space --nindent=1 --renderer-workaround --set-c-precision=6 --protect-ids-noninkscape  --disable-simplify-colors  --keep-editor-data >foobar098
+ /data/project/svgworkaroundbot/prgm2/pythonJK/PythonIn/bin/python3.7 -m scour.scour -i $i -o $i2 --keep-unreferenced-defs --remove-descriptions --strip-xml-space  --set-precision=6 --indent=space --nindent=1 --renderer-workaround --set-c-precision=6 --protect-ids-noninkscape  --disable-simplify-colors  --keep-editor-data >foobar097
+ /data/project/svgworkaroundbot/prgm2/pythonJK/PythonIn/bin/python3.7 -m scour.scour -i tmpJK2.svg -o tmpJK23.svg --keep-unreferenced-defs --remove-descriptions --strip-xml-space  --set-precision=6 --indent=space --nindent=1 --renderer-workaround --set-c-precision=6 --protect-ids-noninkscape  --disable-simplify-colors  --keep-editor-data >foobar098
  cp $i2 Output097.svg
  #cp $i $i2
 echo $i, $i2, $i3

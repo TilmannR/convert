ed5cf1ab6c6c2f043f3ea47c19709295affdf353
Johannes Kalliauer <jo.spam89@gmx.at>
2020-09-12 16:29:11 +0200

    Update

--- gitpush.sh
+++ gitpush.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+chmod u+rx *.sh
+chmod u+r *
+rm -f *.svg
 git config --global core.eol lf
 git config core.eol lf
 #git status
@@ -6,6 +9,12 @@ git add .
 git status
 read -p "Press enter to continue" #Press Control C for stop
 git status
+    read -p "Do you wish to proceed?" yn
+    case $yn in
+        [Yy]* ) git commit -m "Update";git push;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
 git commit -m "Update"
 git status
 git push
